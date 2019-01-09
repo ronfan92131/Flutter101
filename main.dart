3bdc76778a888cb6df92359b95fcd968b1ae07e6
Ron Fan <ronfan92131@gmail.com>
2019-01-08 22:16:50 -0800

    more UI
    1. add a button at the top, as a child of a container, with margin 10. 
    2. add list of cards.  each card has an image and a text description

--- main.dart
+++ main.dart
@@ -6,10 +6,32 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        home: Scaffold(
-      appBar: AppBar(
-        title: Text('Easy'),
-      ),
-    ));
+      home: Scaffold(
+          appBar: AppBar(
+            title: Text('EasyList'),
+          ),
+          body: Column(
+            children: [
+              Container(
+                margin: EdgeInsets.all(10.0),
+                child: RaisedButton(
+                  onPressed: () {},
+                  child: Text('Add Product'),
+                ),
+              ),
+       
+              Card(
+                child: Column(
+                  children: <Widget>[
+                    Image.network(
+                      'https://raw.githubusercontent.com/flutter/website/master/src/_includes/code/layout/lakes/images/lake.jpg',
+                    ),
+                    Text('Mountain Paradise')
+                  ],
+                ),
+              ),
+            ],
+          )),
+    );
   }
 }
